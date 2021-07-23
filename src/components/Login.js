98b56313850884a99c413d4c46ca338ebba9f092
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-07-23 15:20:02 -0400

    AxiosWithAuth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,58 @@
-import React, { useEffect, useState } from "react";
+import React, { useState} from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   const [data,setData]=useState({username:'',password:''})
-  const [error,setError]=useState('')
+  const [error,setError] =useState({error:''})
+  
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
+  // const error = "";
   //replace with error state
-
+  
   const handleChange = (e) =>{
     setData({...data,[e.target.name]:e.target.value})
   }
+const onSubmit = (e) =>{
+  e.preventDefault();
 
+  axios.post('http://localhost:5000/api/login',data)
+  .then(res =>{
+    localStorage.setItem('token',res.data.payload)
+    props.history.push('/bubbles-page')
+  })
+  .catch(err=>{
+    console.log('my error',err)
+    setError({error:'Username or Password not valid.'})
+  })
+  
+}
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
-        <form>
-          <label>
-            <input type='text' name='username' value={data.username} onChange={handleChange}  />
+        <form onSubmit={onSubmit}>
+          <label>Username:
+            <input id='username' type='text' name='username' value={data.username} onChange={handleChange} data-testid="username" />
           </label>
-          <label>
-            <input type='text' name='password' value={data.password} onChange={handleChange}  />
+          <label>Password:
+            <input id='password' type='text' name='password' value={data.password} onChange={handleChange} data-testid="password" />
           </label>
+          <button>Log In!</button>
         </form>
       </div>
 
-      <p data-testid="errorMessage" className="error">{error}</p>
+      <p data-testid="errorMessage" className="error">{error.error}</p>
     </div>
   );
 };
 
 export default Login;
-
 //Task List:
 //1. Build a form containing a username and password field.
 //2. Add whatever state nessiary for form functioning.
 //3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
 //4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
-//5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
+//5. If the username / password is equal to Lambda / School, save that token to localStorage.
