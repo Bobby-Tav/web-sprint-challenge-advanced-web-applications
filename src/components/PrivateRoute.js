4863d08be02111c7accbdb29e7b6ee961b751442
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-07-23 15:49:39 -0400

    MVP

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,5 +1,3 @@
-//Task List:
-//1. Build a PrivateRoute component that redirects if user is not logged in
 
 import React from 'react';
 import { Route,Redirect } from 'react-router-dom';
@@ -11,8 +9,9 @@ const PrivateRoute = ({component:Component,...rest}) =>{
         }else{
             return <Redirect to='/' />
         }
-
     }}/>
 }
-
 export default PrivateRoute;
+
+//Task List:
+//1. Build a PrivateRoute component that redirects if user is not logged in
